62442de8e5adee763334899fe5a023cd749afa80
yian529 <49156840+yian529@users.noreply.github.com>
2020-07-23 12:54:45 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
+  
+// 设置基础值  
+// 根据前端的跨域方式做调整  /yian/mi : /api/yian/mi => /yian/mi
+axios.defaults.baseURL='/api';
+// 设置超时时间
+axios.defaults.timeout=8000;
+// 接口错误拦截
+axios.interceptors.response.use(function(response){
+  // 获取到接口返回的值
+  let res=response.data;
+  if(res.status==0){
+    // 成功
+    return res.data;
+  }else if(res.status==10){
+    // 登录时的接口报错  跳转到登录页面
+    // 使用window.location.href跳转的原因：
+    // 在main.js中用路由跳没有用，路由是挂载到vue实例中，在每一个页面里才能用this.$route.push，在这里this并没有指向vue
+    window.location.href='/#/login'
+  }else{
+    // 报错
+    alert(res.msg);
+  }
+});
 
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
 
 new Vue({
