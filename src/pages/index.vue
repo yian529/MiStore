dc3862693cfd0051b47f11f8dfe3c4ee90ca506b
yian529 <49156840+yian529@users.noreply.github.com>
2020-07-25 09:24:24 +0800

    首页轮播菜单功能

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,62 @@
   <div class="index">
     <div class="container">
       <div class="swiper-box">
+        <!-- 导航菜单 -->
+        <div class="nav-menu">
+          <ul class="menu-wrap">
+            <li class="menu-item">
+              <a class="item-list" href="javascript:;">手机 电话卡</a>
+              <div class="children">
+                <ul v-for="(item,index) in menuList" :key="index">
+                  <li v-for="(sub,i) in item" :key="i">
+                    <a :href="sub?'/#/product'+sub.id:''">
+                      <img :src="sub?sub.img:'/imgs/item-box-2.png'" />
+                      {{sub?sub.name:'小米8青春版'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a class="item-list" href="javascript:;">电视 盒子</a>
+              <div class="children">
+                <ul v-for="(item,index) in menuList_second" :key="index">
+                  <li v-for="(sub,i) in item" :key="i">
+                    <a :href="sub?'/#/product'+sub.id:''">
+                      <img :src="sub?sub.img:'/imgs/item-box-6.jpg'" />
+                      {{sub?sub.name:'小米电视5 75英寸'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li class="menu-item">
+              <a class="item-list" href="javascript:;">笔记本 平板</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a class="item-list" href="javascript:;">家电 插线板</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a class="item-list" href="javascript:;">出行 穿戴</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a class="item-list" href="javascript:;">耳机 音箱</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a class="item-list" href="javascript:;">电源 配件</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a class="item-list" href="javascript:;">生活 箱包</a>
+              <div class="children"></div>
+            </li>
+          </ul>
+        </div>
+        <!-- 轮播插件 -->
         <swiper ref="mySwiper" :options="swiperOptions">
           <swiper-slide v-for="(item,index) in slideList" :key="index">
             <a :href="'/#/product/'+item.id">
@@ -53,7 +109,7 @@ export default {
         pagination: {
           el: ".swiper-pagination",
           // clickable设置为true时，点击分页器的指示点分页器会控制Swiper切换
-          clickable :true
+          clickable: true
         },
         // 前进后退按钮
         navigation: {
@@ -82,6 +138,66 @@ export default {
           id: "",
           img: "/imgs/slider/slide-5.jpg"
         }
+      ],
+      // 轮播菜单功能的实现
+      // 定义两个数组，一个二维数组，循环两次，横向循环六次，纵向循环四次
+      menuList: [
+        [
+          {
+            id: 30,
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9"
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版"
+          },
+          {
+            id: 32,
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 Pro"
+          },
+          {
+            id: 33,
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G专区"
+          }
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      menuList_second: [
+        [
+          {
+            id: 30,
+            img: "/imgs/item-box-5.jpg",
+            name: "小米电视 大师 65英寸"
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-6.jpg",
+            name: "小米电视5 75英寸"
+          },
+          {
+            id: 32,
+            img: "/imgs/item-box-7.jpg",
+            name: "全面屏电视Pro 55英寸"
+          },
+          {
+            id: 33,
+            img: "/imgs/item-box-8.jpg",
+            name: "小米电视4A 58英寸"
+          }
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
       ]
     };
   }
@@ -89,13 +205,88 @@ export default {
 </script>
 
 <style lang="scss" scope>
+@import "../assets/scss/config.scss";
+@import "../assets/scss/mixin.scss";
 .index {
   .swiper-box {
+    .nav-menu {
+      position: absolute;
+      z-index: 10;
+      width: 264px;
+      height: 451px;
+      background-color: rgba(85, 88, 90, 0.486);
+      padding: 26px 0;
+      box-sizing: border-box;
+      .menu-wrap {
+        .menu-item {
+          height: 50px;
+          line-height: 50px;
+          padding-left: 30px;
+          .item-list {
+            // a标签是行内元素，没有填满；li标签是填满的
+            // 让a变为块级元素可以实现a与箭头在li中被填满，从而排在一行
+            display: block;
+            position: relative;
+            color: $colorG;
+            font-size: 16px;
+            &::after {
+              position: absolute;
+              right: 30px;
+              top: 17.5px;
+              content: " ";
+              @include bgImg(10px, 15px, "/imgs/icon-arrow.png");
+            }
+          }
+          &:hover {
+            background-color: $colorA;
+            transition: all 0.9s linear;
+            .children {
+              display: block;
+            }
+          }
+          .children {
+            display: none;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            width: 962px;
+            height: 451px;
+            background-color: $colorG;
+            border-top: 1px solid $colorH;
+            ul {
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              line-height: 75px;
+              li {
+                flex: 1;
+                height: 75px;
+                padding-left: 23px;
+                a {
+                  font-size: $fontJ;
+                  color: $colorB;
+                  img {
+                    width: 35px;
+                    height: 42px;
+                    margin-left: 10px;
+                    vertical-align: middle;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
     .swiper-container {
       height: 451px;
       img {
         width: 100%;
         height: 100%;
+      }
+      .swiper-button-prev {
+        // 解决前进按钮被导航菜单挡住的问题
+        left: 274px;
       }
     }
   }
